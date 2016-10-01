aeefc4f9649c72caff023cdabb796eeacd9d2305
foxsofter <foxsofter@gmail.com>
2016-10-01 21:01:15 +0800

    test pod

--- Classes/Mvvm/ViewControllers/LPDScrollViewController.h
+++ Classes/Mvvm/ViewControllers/LPDScrollViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MJRefresh/MJRefresh.h>
+//#import <MJRefresh/MJRefresh.h>
+#import "UIScrollView+MJRefresh.h"
+#import "UIScrollView+MJExtension.h"
+#import "UIView+MJExtension.h"
+
+#import "MJRefreshNormalHeader.h"
+#import "MJRefreshGifHeader.h"
+
+#import "MJRefreshBackNormalFooter.h"
+#import "MJRefreshBackGifFooter.h"
+#import "MJRefreshAutoNormalFooter.h"
+#import "MJRefreshAutoGifFooter.h"
+
 #import "LPDViewController.h"
 #import "LPDScrollViewControllerProtocol.h"
 
